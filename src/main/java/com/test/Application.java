ee5422609efa3375e75bcf72434d802706bf5777
zhangbowen <zhangbowen@bcbook.cn>
2019-11-20 12:35:41 +0800

    add: 后台管理接口端项目创建

---
+++ src/main/java/com/test/Application.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangbowen
+ * @since 2019-11-20
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
